166ab102e0e73943dd8fba076ac2ede760fbc4d7
luulong83 <luucangkimlong83@yahoo.com.vn>
2018-03-07 21:10:56 +0700

    a
    a

--- Load/frmExportVinabiz.cs
+++ Load/frmExportVinabiz.cs
@@ -166,7 +166,7 @@ namespace Load
                 {
                     strTieuDe = ((DataRowView)checkedListBox1.CheckedItems[0]).Row.ItemArray[1].ToString();
                     strsql = string.Format(" select  " + strcolumn + "	from {0}.dbo.vinabiz a left join {0}.dbo.dm_vinabiz b on a.danhmucid= b.id " +
-                                      "  where a.ttlh_tinhid in({1}) and (CONVERT(DATETIME,ttdk_ngaycap,103) >= ''{2}'' and CONVERT(DATETIME,ttdk_ngaycap,103) <''{3}'') order by a.danhmucid ", strdatabasename, getstringListTinh(),dateTimePicker1.Text,dateTimePicker2.Text);
+                                      "  where a.ttlh_tinhid in({1}) and (CONVERT(DATETIME,ttdk_ngaycap,103) >= ''{2}'' and CONVERT(DATETIME,ttdk_ngaycap,103) <=''{3}'') order by a.danhmucid ", strdatabasename, getstringListTinh(),dateTimePicker1.Text,dateTimePicker2.Text);
                 }
                 else
                 {
